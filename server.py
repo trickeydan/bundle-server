6c6a599c921ee121e5ae9fdc4651db81e964b7a2
Dan Trickey <dan@trickey.io>
2018-01-27 11:40:35 +0000

    Added some templating stuff and error handling

--- server.py
+++ server.py
@@ -3,14 +3,14 @@
 # Bundle Server
 # Written in Flask to provide an API
 
-# (c) 2017 D.G.Trickey and S.J.McCreery
+# Written by D.G.Trickey and S.J.McCreery
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def vote():
-    return 'User voting page'
+    return render_template('vote.html')
 
 @app.route('/explore')
 def explore():
@@ -25,3 +25,11 @@ def api_vote():
     if request.method == 'POST':
         return request.form['song_id']
     return "JSSSONNNNN"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Um, that doesn't exist"
+
+@app.errorhandler(405)
+def page_not_found(error):
+    return "Incorrect Request Type"
